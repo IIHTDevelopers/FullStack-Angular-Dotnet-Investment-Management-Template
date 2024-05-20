d01b75e03770655d7612ce2c4d62bc09a26d85e4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-05-20 19:52:09 +0530

    Update investment-planning.service.ts

--- Frontend/src/app/services/investment-planning.service.ts
+++ Frontend/src/app/services/investment-planning.service.ts
@@ -1,37 +1,37 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Investment } from '../models/investment-planning.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvestmentService {
-  private apiUrl = 'http://localhost:5000';
+  private apiUrl = '';
 
   constructor(private http: HttpClient) { }
 
-  getAllInvestments(): Observable<Investment[]> {
-    return this.http.get<Investment[]>(`${this.apiUrl}/get-all-investments`);
+  getAllInvestments(): any {
+    // write your logic here
+    return null;
   }
 
-  getInvestmentById(id: number): Observable<Investment> {
-    return this.http.get<Investment>(`${this.apiUrl}/get-Investment-by-id?id=${id}`);
+  getInvestmentById(id: number): any {
+    // write your logic here
+    return null;
   }
 
-  createInvestment(investment: Investment): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/create-investment`, investment, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    });
+  createInvestment(investment: Investment): any {
+    // write your logic here
+    return null;
   }
 
-  updateInvestment(investment: Investment): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/update-investment`, investment, {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    });
+  updateInvestment(investment: Investment): any {
+    // write your logic here
+    return null;
   }
 
-  deleteInvestment(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/delete-Investment?id=${id}`);
+  deleteInvestment(id: number): any {
+    // write your logic here
+    return null;
   }
 }
